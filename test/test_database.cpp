be0d9b9ecc2d884c6e60df1e840668a9e1aea320
switchblade <terekhin.mikhail.01@gmail.com>
2023-04-04 07:31:12 -0700

    Updated database test

--- test/test_database.cpp
+++ test/test_database.cpp
@@ -9,17 +9,13 @@
 int main()
 {
 	const auto old_db = reflex::type_database::instance();
+	TEST_ASSERT(!reflex::type_info::get("int").valid());
 
 	reflex::type_info::reflect<int>();
 	TEST_ASSERT(reflex::type_info::get("int").valid());
 
-	auto new_db0 = reflex::type_database{std::move(*old_db)};
-	TEST_ASSERT(reflex::type_database::instance(&new_db0) == old_db);
-	TEST_ASSERT(reflex::type_database::instance() == &new_db0);
+	auto new_db = reflex::type_database{std::move(*old_db)};
+	TEST_ASSERT(reflex::type_database::instance(&new_db) == old_db);
+	TEST_ASSERT(reflex::type_database::instance() == &new_db);
 	TEST_ASSERT(reflex::type_info::get("int").valid());
-
-	auto new_db1 = reflex::type_database{};
-	TEST_ASSERT(reflex::type_database::instance(&new_db1) == &new_db0);
-	TEST_ASSERT(reflex::type_database::instance() == &new_db1);
-	TEST_ASSERT(!reflex::type_info::get("int").valid());
 }
